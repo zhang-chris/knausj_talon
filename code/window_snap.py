9f3ebdba9f18ebc22def325300512a08bf7dccd6
Christopher Zhang <chris.zhang@berkeley.edu>
2021-04-22 12:50:01 -0700

    customize snap

--- code/window_snap.py
+++ code/window_snap.py
@@ -168,7 +168,7 @@ _snap_positions = {
     "bottom right two thirds": RelativeScreenPos(1 / 3, 0.5, 1, 1),
     "bottom center third": RelativeScreenPos(1 / 3, 0.5, 2 / 3, 1),
     # Special
-    "center": RelativeScreenPos(1 / 8, 1 / 6, 7 / 8, 5 / 6),
+    "center": RelativeScreenPos(1 / 8, 0, 7 / 8, 1),
     "full": RelativeScreenPos(0, 0, 1, 1),
     "fullscreen": RelativeScreenPos(0, 0, 1, 1),
 }
